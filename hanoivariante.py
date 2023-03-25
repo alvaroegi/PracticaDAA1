8bac589275fc5116c67f2b269c28367c88d47767
alvaroegi <alvaro.egido2@gmail.com>
2023-03-25 12:38:16 +0100

    25-03

--- hanoivariante.py
+++ hanoivariante.py
@@ -3,15 +3,22 @@ def hanoi(n,o,d,a):
         print("Mueve disco",n,"desde torre",o,"a torre",a)
         print("Mueve disco",n,"desde torre",a,"a torre",d)
     else:
-        hanoi(n-1,o,a,d)
+        hanoi(n-1,o,d,a)
         print("Mueve disco",n,"desde torre",o,"a torre",a)
-        hanoi(n-1,d,a,o)
+        hanoi(n-1,d,o,a)
         print("Mueve disco", n, "desde torre", a, "a torre", d)
-        hanoi(n-1,a,d,o)
+        hanoi(n-1,o,d,a)
 
 # Ejemplo de uso
-hanoi(2,1,2,3)
-
-
-
+def casoDePrueba():
+    try:
+        num = int(input())
+        if(num > 0) and (num <= 9):
+            hanoi(num,1,3,2)
+            return True
+    except:
+        return False
+if __name__ == "__main__":
+    while casoDePrueba():
+        pass
 
